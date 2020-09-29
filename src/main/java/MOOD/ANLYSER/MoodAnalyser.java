527e4ec2e9d9068fe8ad8f855ce94d7ef8789bbb
keshavanandsingh <keshavanand22@gmail.com>
2020-09-30 00:39:42 +0530

    TC1.1 added

--- src/main/java/MOOD/ANLYSER/MoodAnalyser.java
+++ src/main/java/MOOD/ANLYSER/MoodAnalyser.java
@@ -1,30 +1,14 @@
 package MOOD.ANLYSER;
-import com.training.InvalidMoodEx.ExceptionType;
 
-
-public class MoodAnalyser 
+public class MoodAnalyser
 {
 	private String message;
-	public MoodAnalyser() {
-	}
-	public MoodAnalyser(String string) {
-		this.message=string;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public String AnalyseMood() throws InvalidMoodEx {
-			try {
-			if(message.length()==0)
-				throw new InvalidMoodEx(InvalidMoodEx.ExceptionType.Entered_Empty, "Enter A Valid Mood !");
-			if(message.contains("Sad"))
+	public MoodAnalyser() {}
+		public String AnalyseMood(String string) {
+			if(string.contains("Sad"))
 				return "Sad";
 			else
 				return "Happy";
-			}
-			catch(Exception e){
-				throw new InvalidMoodEx(InvalidMoodEx.ExceptionType.Entered_Null, "Enter A Valid Mood !");
-			}
 			
 		}
 }
